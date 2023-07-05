61b433fbb39669bb48ecf93ea60dd021e5d733fd
as575909 <as575909@gmail.com>
2023-07-05 18:49:46 +0530

    password

--- src/components/UIcontrols/PasswordInput/util.ts
+++ src/components/UIcontrols/PasswordInput/util.ts
@@ -8,5 +8,11 @@ export const passwordRegexTypes: PasswordRegexType = {
   numeric: /^[0-9]+$/, // Password must contain only numeric characters
   alphanumeric: /^[a-zA-Z0-9]+$/, // Password must contain only alphanumeric characters
   letters: /^[a-zA-Z]+$/, // Password must contain only letters (no numbers or special characters)
-  // Add more regex patterns as needed
+  uppercaseLowercaseDigit: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]+$/,  // Password must contain at least one lowercase letter (a-z), one uppercase letter (A-Z), and one digit (\d).
+  uppercaseLowercaseDigitSpecial: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[a-zA-Z\d!@#$%^&*]+$/,   // Password must contain at least one lowercase letter (a-z), one uppercase letter (A-Z), one digit (\d), and one special character (!@#$%^&*).
+  minLengthWithSpaces: /^[\s\S]{6,}$/,   // Password must be at least 6 characters long, including spaces.
+  allowedSpecialChars: /^[a-zA-Z0-9!@#$%^&*]+$/,  // Password can contain only alphanumeric characters (a-z, A-Z, 0-9), and the following special characters: !@#$%^&*.
+  noSpacesOrSpecialChars: /^[a-zA-Z0-9]+$/,  // Password must contain only alphanumeric characters (a-z, A-Z, 0-9) and no spaces or special characters.
+
+  
 };
